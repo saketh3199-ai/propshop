415f186a9e87b2319dbfe94fec23d0fe19b758c4
Saketh Pavan <saketh3199@gmail.com>
2026-08-25 17:28:22 +0530

    Configure DNS for MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-// import dns from "dns"; // added this
+import dns from "dns"; // added this
 
-// dns.setServers(["8.8.8.8", "8.8.4.4"]); //added this to resolve the issue with connecting express and mongoose
+dns.setServers(["8.8.8.8", "8.8.4.4"]); //added this to resolve the issue with connecting express and mongoose
 
 
 const connectDB = async ()=>
 {
     try
     {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {tls: true,tlsAllowInvalidCertificates: true})
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected : ${conn.connection.host}`)
     }
    catch(error)
